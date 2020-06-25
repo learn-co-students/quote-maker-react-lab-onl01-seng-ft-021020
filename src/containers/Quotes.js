db7114af8430b21675404a361dd1c18457500631
biagioo <mendolia2328@gmail.com>
2020-06-25 14:14:07 -0400

    Done.

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import QuoteCard from '../components/QuoteCard';
+// import QuoteCard from '../components/QuoteCard';
 
 class Quotes extends Component {
 
   render() {
+    console.log(this.props)
     return (
       <div>
         <hr />
@@ -20,6 +21,8 @@ class Quotes extends Component {
 
                 Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
                */}
+
+
             </div>
           </div>
         </div>

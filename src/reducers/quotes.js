d8d9593428fbdb8b5a85f7b719466072efa30a30
biagioo <mendolia2328@gmail.com>
2020-06-25 14:22:25 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -2,6 +2,15 @@ export default (state = [], action) => {
   switch(action.type){
     case 'ADD_QUOTE':
       return(state.concat(action.quote))
+
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId)
+
+    case 'UPVOTE_QUOTE':
+      return state
+
+    case 'DOWNVOTE_QUOTE':
+      return state
     default :
       return state
   }
